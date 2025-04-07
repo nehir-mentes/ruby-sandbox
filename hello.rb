b6ba2e05a42287dda785e2562313423e64486f06
nehir-mentes <nehirmen.98@gmail.com>
2025-04-07 18:58:22 +0000

    ss

--- hello.rb
+++ hello.rb
@@ -4,3 +4,22 @@ pp "world!"
 require "./goodbye.rb"
 
 pp "after"
+
+require "date"
+x = Date.today
+
+pp x.month
+d = x.day
+
+remainder = d % 10
+
+if remainder == 0
+  suffix = "th"
+elsif remainder == 1
+  suffix = "nd"
+elsif remainder == 2
+end
+
+require "active_support/all"
+
+pp "Hello! It is the #{x.day.ordinalize} th of April"
